fb91b4c78aed92de0c529cfd2e495346c4eef32a
Jorge Gómez Tortosa <gmzjordi@gmail.com>
2022-04-28 11:14:30 +0200

    Users crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AreasModule } from './areas/areas.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AreasModule],
+  imports: [AreasModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
